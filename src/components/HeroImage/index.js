8fc3ef8b93fc3646ae078e3abd0c30137dff1921
notPesol <pesol2012@hotmail.com>
2021-08-02 18:17:26 +0700

    go back home

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,14 @@
+// styles
+import { Wrapper, Content, Text } from './HeroImage.style';
+
+import React from 'react'
+
+function HeroImage() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+
+export default HeroImage
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,15 @@
-import React, { useState, useEffect } from 'react';
-
-// APi
-import API from '../API';
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // components
 
 // hook
-
+import { useHomeFetch } from './hooks/useHomeFetch'
 // images
 import NoImg from '../images/no_image.jpg';
 
 function Home() {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMoveis = async (page, searchTerm = '') => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-      setState(prev => (
-        {
-          ...movies,
-          results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-        }
-      ));
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    fetchMoveis(1);
-  }, []);
+  const { state, loading, error } = useHomeFetch();
 
   console.log(state);
 
